51c80e6704c20661d7dfbf809db61ebed3af7749
Mambetova Zarina <zarina.mambetova84@gmail.com>
2025-08-21 22:34:37 +0300

    Практика jest mock, async, axios

--- 03-async/async.test.js
+++ 03-async/async.test.js
@@ -1,13 +1,73 @@
-// экспортируем  из отслеживаемого файла
-const Lodash = require('./sync');
+const axios = require('axios');
+const Ajax = require('./async');
 
+jest.mock('axios')
 
-describe('Lodash compact:', () => {
+describe('Ajax: echo', () => {
 
+  test('should return value async', async () => {
+    const result = await Ajax.echo('some data')
+    expect(result).toBe('some data')
+  })
+  // ловим ошибку
+  test('should catch error with async',
+    async () => {
+      try {
+        await Ajax.echo()
+      } catch (e) {
+        expect(e.message).toBe('error')
+      }
 
-  test('should be defined', () => {
-    expect(_.compact).toBeDefined()
-    // или
-    expect(_.compact).not.toBeUndefined()
+    })
+
+  // Или так, без async
+  test('should return value promise',
+    () => {
+      return Ajax.echo('some data').then(data => {
+        expect(data).toBe('some data')
+      })
+
+    })
+
+  test('should catch error with promise',
+    () => {
+      return Ajax.echo().catch(err => {
+        expect(err).toBeInstanceOf(Error)
+      })
+    })
+})
+
+describe('Ajax: get', () => {
+  let response
+  let todos
+
+  beforeEach(() => {
+    todos = [{ id: 1, title: 'Todo 1', completed: false }]
+    response = {
+      data: {
+        todos
+      }
+    }
+  })
+
+  test('should return data  from backend', () => {
+    // Для тестирования не нужно, чтобы функция обращалась к бэку,
+    //  нужно отловить этот момент начала запроса.
+    // response сами определяем в beforeEach
+    axios.get.mockReturnValue(response)
+
+    return Ajax.get().then(data => {
+      expect(data.todos).toEqual(todos)
+    })
+  })
+
+  test('should return data  from backend async', async () => {
+    // Для тестирования не нужно, чтобы функция обращалась к бэку,
+    //  нужно отловить этот момент начала запроса.
+    // response сами определяем в beforeEach
+    axios.get.mockReturnValue(response)
+
+    const data = await Ajax.get()
+    expect(data.todos).toEqual(todos)
   })
 })
